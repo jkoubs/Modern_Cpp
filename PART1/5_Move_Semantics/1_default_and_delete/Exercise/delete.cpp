e187da7b87ce27426c65a09bc5604338c38da8b4
Jason Koubi <jazkoubi@gmail.com>
2025-03-27 15:35:37 -0700

    delete exercise

---
+++ PART1/5_Move_Semantics/1_default_and_delete/Exercise/delete.cpp
@@ -0,0 +1,20 @@
+struct OnlyInt {
+  OnlyInt(int) {}
+
+  template <typename T> OnlyInt(T) = delete;
+};
+
+int main() {
+
+  OnlyInt(5);
+  OnlyInt(5L);
+  OnlyInt(5LL);
+  OnlyInt(5UL);
+  OnlyInt(5.5);
+  OnlyInt('5');
+  OnlyInt(true);
+}
+
+/*
+GOAL: Define a class template, that can be invoked with an int value
+*/
