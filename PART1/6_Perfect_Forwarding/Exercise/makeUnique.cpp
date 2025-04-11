59c699093d023556f27a903e76d51b9a0f3432b1
Jason Koubi <jazkoubi@gmail.com>
2025-04-10 17:31:50 -0700

    perfect forwarding

---
+++ PART1/6_Perfect_Forwarding/Exercise/makeUnique.cpp
@@ -0,0 +1,7 @@
+/*
+
+Review the generic factory function in 1_perfectForwarding.cpp and use it as a
+reference to implement the makeUnique factory fct.
+
+The makeUnique fct should generate a std::unique_ptr for any given type.
+*/
